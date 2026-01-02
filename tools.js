26ff1ad3844a688a1912ae56b32aad8eeeef0bd3
name <email@git.com>
2026-01-02 16:35:47 -0700

    Added skin rendering + card js generation to index Added two files that for now do nothing but I plan to intergrate latter

---
+++ tools.js
@@ -0,0 +1,22 @@
+//input JSON
+function verifyJSON() {
+    //consider adding support of aditional info
+
+}
+//input string
+function stringToJSON(){
+    //do conversion
+
+    verifyJSON();
+
+}
+//now we have a JOSN obj that is the skin
+
+
+//extract as string
+function JSONtoString() {
+    //print string
+}
+function JSONOut() {
+    //print JSON
+}
